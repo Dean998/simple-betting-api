5c2b987a2daafa02c60a4d3897a7a11a6a3a0de3
Dean <dean.b.98@hotmail.com>
2025-05-07 11:08:11 +0100

    feat: generate betting module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BettingModule } from './betting/betting.module';
 
 @Module({
-  imports: [],
+  imports: [BettingModule],
   controllers: [AppController],
   providers: [AppService],
 })
