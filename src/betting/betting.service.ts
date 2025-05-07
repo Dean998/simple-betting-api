95247e9b3e82e79d5959d3c5d690917cadeb5ee2
Dean <dean.b.98@hotmail.com>
2025-05-07 11:19:57 +0100

    feat: update service to handle create and find all markets based on sportType or eventStatus

--- src/betting/betting.service.ts
+++ src/betting/betting.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { BettingMarket, EventStatus, SportType } from './betting.interface';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class BettingService {}
+export class BettingService {
+  private bets: BettingMarket[] = [];
+
+  create(market: Omit<BettingMarket, 'id'>): BettingMarket {
+    const newMarket: BettingMarket = {
+      ...market,
+      id: randomUUID(),
+    };
+    this.bets.push(newMarket);
+    return newMarket;
+  }
+
+  findAll(filter?: {
+    sportType?: SportType;
+    eventStatus?: EventStatus;
+  }): BettingMarket[] {
+    return this.bets.filter((market) => {
+      if (filter?.sportType && market.sportType !== filter.sportType)
+        return false;
+      if (filter?.eventStatus && market.eventStatus !== filter.eventStatus)
+        return false;
+      return true;
+    });
+  }
+}
