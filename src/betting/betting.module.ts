4028bff40b9888b795f9390a8789a9e97ad975f1
Dean <dean.b.98@hotmail.com>
2025-05-07 11:39:42 +0100

    feat: incorporate updating odds and emitting event when odds are updated

--- src/betting/betting.module.ts
+++ src/betting/betting.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BettingService } from './betting.service';
 import { BettingController } from './betting.controller';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
+  imports: [EventEmitterModule.forRoot()],
   providers: [BettingService],
-  controllers: [BettingController]
+  controllers: [BettingController],
 })
 export class BettingModule {}
--- src/betting/betting.service.ts
+++ src/betting/betting.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { BettingMarket, EventStatus, SportType } from './betting.interface';
 import { randomUUID } from 'crypto';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 
 @Injectable()
 export class BettingService {
-  private bets: BettingMarket[] = [];
+  private bettingMarkets: BettingMarket[] = [];
+  private readonly logger = new Logger(BettingService.name);
+
+  constructor(private readonly eventEmitter: EventEmitter2) {}
 
   create(market: Omit<BettingMarket, 'id'>): BettingMarket {
     const newMarket: BettingMarket = {
       ...market,
       id: randomUUID(),
     };
-    this.bets.push(newMarket);
+    this.bettingMarkets.push(newMarket);
     return newMarket;
   }
 
@@ -19,12 +23,43 @@ export class BettingService {
     sportType?: SportType;
     eventStatus?: EventStatus;
   }): BettingMarket[] {
-    return this.bets.filter((market) => {
+    return this.bettingMarkets.filter((market) => {
       if (filter?.sportType && market.sportType !== filter.sportType)
         return false;
       if (filter?.eventStatus && market.eventStatus !== filter.eventStatus)
         return false;
       return true;
     });
+  }
+  findOne(id: string): BettingMarket | undefined {
+    return this.bettingMarkets.find((market) => market.id === id);
+  }
+
+  updateOdds(marketId: string, updatedOdds: number): BettingMarket {
+    const bettingMarket = this.bettingMarkets.find(
+      (market) => market.id === marketId,
+    );
+
+    if (!bettingMarket) {
+      this.logger.error(`Betting market with ID ${marketId} not found.`);
+      throw new Error(`Betting market with ID ${marketId} not found.`);
+    }
+
+    bettingMarket.odds = updatedOdds;
+
+    this.eventEmitter.emit('odds.updated', bettingMarket);
+
+    this.logger.log(
+      `Event emitted: Odds for betting market ${bettingMarket.id} updated to ${updatedOdds} from ${bettingMarket.odds}`,
+    );
+
+    return bettingMarket;
+  }
+
+  @OnEvent('odds.updated')
+  handleOddsUpdatedEvent(bettingMarket: BettingMarket) {
+    this.logger.log(
+      `Event received: odds updated for market ${bettingMarket.id}, new odds: ${bettingMarket.odds}`,
+    );
   }
 }
